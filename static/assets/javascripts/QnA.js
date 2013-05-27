dbd587f70cdea24af2e4b8ce74a54135b2869ea2
chenwl <qzchenwl@gmail.com>
2013-05-27 15:18:25 +0800

    tab for textarea

--- static/assets/javascripts/QnA.js
+++ static/assets/javascripts/QnA.js
@@ -126,6 +126,29 @@ function registerAll() {
         showQuestion({id: id});
         return false;
     });
+
+    // allow tab for textarea
+    $("textarea").keydown(function(e) {
+    if(e.keyCode === 9) { // tab was pressed
+        // get caret position/selection
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        var $this = $(this);
+        var value = $this.val();
+
+        // set textarea value to: text before caret + tab + text after caret
+        $this.val(value.substring(0, start)
+            + "\t"
+            + value.substring(end));
+
+        // put caret at right position again (add one for the tab)
+        this.selectionStart = this.selectionEnd = start + 1;
+
+        // prevent the focus lose
+        e.preventDefault();
+    }
+});
 }
 // END register listeners
 
